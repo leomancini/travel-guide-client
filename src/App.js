28e891a36195e59413519394cf802bad1550472b
Leo Mancini <mancini@leo.gd>
2024-12-23 22:25:07 +0800

    Fixing variable names

--- src/App.js
+++ src/App.js
@@ -99,12 +99,12 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    const favicon =
+    const faviconElement =
       document.querySelector('link[rel="icon"]') ||
       document.createElement("link");
-    favicon.rel = "icon";
-    favicon.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${favicon}</text></svg>`;
-    document.head.appendChild(favicon);
+    faviconElement.rel = "icon";
+    faviconElement.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${favicon}</text></svg>`;
+    document.head.appendChild(faviconElement);
   }, [favicon]);
 
   useEffect(() => {
